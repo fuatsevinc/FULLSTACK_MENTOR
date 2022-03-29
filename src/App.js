a08625e71963bc309c577a22795723e6164719e2
fuatsevinc <fuatsevinc@icloude.com>
2022-03-29 22:57:34 +0200

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="www.fuatsevinc.com"
+          href="fuatsevinc.com"
           target="_blank"
           rel="noopener noreferrer"
         >
