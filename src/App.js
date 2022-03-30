0f64a15e647300782f4a347bcffbdb3874f08009
fuatsevinc <fuatsevinc@icloude.com>
2022-03-30 23:51:31 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import navbar from './compenent/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="fuatsevinc.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fuat Sevinc
-        </a>
-      </header>
+      <navbar/>
     </div>
   );
 }
